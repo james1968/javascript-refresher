872d956f39996deded15e1d77e19279a3e88d102
James <bakerje@mac.com>
2017-02-10 16:16:16 +0000

    Completed up to question 6

--- spec/QuestionsSpec.js
+++ spec/QuestionsSpec.js
@@ -5,27 +5,27 @@ describe('JavaScript Refresher Questions', function() {
     expect(selectElementsStartingWithE(testArray)).toEqual(['emma', 'ethan', 'elise']);
   });
 
-  xit('2. selectElementsStartingWithVowel: return elements from an array starting with a vowel', function() {
+  it('2. selectElementsStartingWithVowel: return elements from an array starting with a vowel', function() {
     testArray = ['open', 'closed', 'small', 'also', 'ice', 'even'];
     expect(selectElementsStartingWithVowel(testArray)).toEqual(['open', 'also', 'ice', 'even']);
   });
 
-  xit('3. removeNullsFromArray: remove null elements and return array', function() {
+  it('3. removeNullsFromArray: remove null elements and return array', function() {
     testArray = [null, null, true, false, 1, null, 2, 'x', null];
     expect(removeNullsFromArray(testArray)).toEqual([true, false, 1, 2, 'x']);
   });
 
-  xit('4. removeNullsAndFalseFromArray: remove null and false elements and return array', function() {
+  it('4. removeNullsAndFalseFromArray: remove null and false elements and return array', function() {
     testArray = [null, null, true, false, 1, null, 2, false, 'x', null];
     expect(removeNullsAndFalseFromArray(testArray)).toEqual([true, 1, 2, 'x']);
   });
 
-  xit('5. reverseEveryElementInArray: return array with each individual string element reversed', function() {
+  it('5. reverseEveryElementInArray: return array with each individual string element reversed', function() {
       testArray = ['coffee', 'tea', 'cake', 'brownie'];
       expect(reverseEveryElementInArray(testArray)).toEqual(['eeffoc', 'aet', 'ekac', 'einworb']);
   });
 
-  xit('6. dropFirstThreeElements: return array with first three elements removed', function() {
+  it('6. dropFirstThreeElements: return array with first three elements removed', function() {
     testArray = [1,2,3,4,5,6]
     expect(dropFirstThreeElements(testArray)).toEqual([4,5,6]);
   });
--- src/Questions1-10.js
+++ src/Questions1-10.js
@@ -13,31 +13,60 @@ function selectElementsStartingWithE(testArray) {
 // Question 2
 
 function selectElementsStartingWithVowel(array) {
+eArray = [];
+	for (i = 0; i < testArray.length; i++) {
+		if (testArray[i].charAt(0) === 'e' ||
+			testArray[i].charAt(0) === 'a' ||
+			testArray[i].charAt(0) === 'i' ||
+			testArray[i].charAt(0) === 'o' ||
+			testArray[i].charAt(0) === 'u') {
 
-};
+				eArray.push(testArray[i]);
+        }
+	}
+	return eArray;
+}
 
 // Question 3
 
 function removeNullsFromArray(array) {
-
+	eArray = [];
+	for (i = 0; i < testArray.length; i++) {
+		if (testArray[i] !== null) {
+			eArray.push(testArray[i]);
+        }
+	}
+	return eArray;
 };
 
 // Question 4
 
 function removeNullsAndFalseFromArray(array) {
-
+	eArray = [];
+	for (i = 0; i < testArray.length; i++) {
+		if (testArray[i] !== null && testArray[i] !== false) {
+			eArray.push(testArray[i]);
+        }
+	}
+	return eArray;
 };
 
 // Question 5
 
-function reverseEveryElementInArray(array) {
+function reverseEveryElementInArray(testArray) {
+	var result = [];
+	for (i = 0; i < testArray.length; i++) {
+		result.push(testArray[i].split('').reverse().join(''));
+	}
+	return result;
+}
 
-};
+
 
 // Question 6
 
 function dropFirstThreeElements(array) {
-
+	return testArray.slice(3, 6);
 };
 
 // Question 7
